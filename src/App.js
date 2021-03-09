5c56796915009370729a12dce839de36c95f312f
Asif Estiak <me.asif.ae@gmail.com>
2021-03-10 01:34:25 +0600

    checking links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,6 +9,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [league, setLeague] = useState([]);
+  useEffect(() => {
+    const url = 'https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=4328';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setLeague(data.leagues[0]))
+    .catch(err => console.log(err));
+  }, []);
+  console.log(league);
+  const [leagues, setLeagues] = useState([]);
+  useEffect(() => {
+    const url = 'https://www.thesportsdb.com/api/v1/json/1/all_leagues.php';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setLeagues(data.leagues))
+    .catch(err => console.log(err));
+  }, []);
+  console.log(leagues);
   return (
     <>
     </>
