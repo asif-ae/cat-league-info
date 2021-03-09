8fb89148db96b51a003ae7d14ca6d2d16a055c8c
Asif Estiak <me.asif.ae@gmail.com>
2021-03-10 01:17:05 +0600

    imported dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
