9f5daad31f854174aec51dbf4d4c674937421586
Asif Estiak <me.asif.ae@gmail.com>
2021-03-10 12:07:42 +0600

    fixed a warning

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
