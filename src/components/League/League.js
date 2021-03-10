1669cb8ff6ef7ff27ba327ed1236dd91ff9105c3
Asif Estiak <me.asif.ae@gmail.com>
2021-03-11 02:28:11 +0600

    added logoes in the home directory

--- src/components/League/League.js
+++ src/components/League/League.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
@@ -7,10 +7,20 @@ import { Link } from 'react-router-dom';
 const League = (props) => {
   const {strLeague, strSport, idLeague} = props.league;
   
+  const [league, setLeague] = useState({});
+  useEffect(() => {
+    const singleLinkURL = `https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${idLeague}`;
+    fetch(singleLinkURL)
+    .then(res => res.json())
+    .then(data => setLeague(data.leagues[0]))
+    .catch(err => console.log(err));
+  }, [idLeague]);
+  console.log(league.strBadge);
+
   return (
     <div className="col-md-6 col-lg-4">
       <div className="p-3 text-center">
-        <img src="" alt=""/>
+        <img src={league.strBadge} alt={strLeague}/>
         <h5>{strLeague}</h5>
         <p><small>Sports type: {strSport}</small></p>
         <Link to={`/league/${idLeague}`}>
