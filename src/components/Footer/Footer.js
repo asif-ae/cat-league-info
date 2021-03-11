43048873964a310277ae083191cdaa255f6b8691
Asif Estiak <me.asif.ae@gmail.com>
2021-03-11 16:32:16 +0600

    added footer in the detail page

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Footer.css';
+import { faFacebookF, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = (props) => {
+  const { strTwitter, strFacebook, strYoutube } = props.league;
+
+  // Social Media link conditions and generate the link
+  const twitterCondition = !strTwitter ? "www.twitter.com" : strTwitter;
+  const twitter = `https://${twitterCondition}`;
+  const facebookCondition = !strFacebook ? "www.facebook.com" : strFacebook;
+  const facebook = `https://${facebookCondition}`;
+  const youtubeCondition = !strYoutube ? "www.youtube.com" : strYoutube;
+  const youtube = `https://${youtubeCondition}`;
+
+  return (
+    <footer className="text-center">
+      <div className="footer-icon">
+        <a href={twitter} target="_blank" rel="noreferrer">
+          <span className="icon twitter">
+            <FontAwesomeIcon icon={faTwitter} />
+          </span>
+        </a>
+        <a href={facebook} target="_blank" rel="noreferrer">
+          <span className="icon facebook">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </span>
+        </a>
+        <a href={youtube} target="_blank" rel="noreferrer">
+          <span className="icon youtube">
+            <FontAwesomeIcon icon={faYoutube} />
+          </span>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/LeagueDetail/LeagueDetail.js
+++ src/components/LeagueDetail/LeagueDetail.js
@@ -1,28 +1,16 @@
-import { faFacebookF, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import './LeagueDetail.css';
 import TopBanner from '../TopBanner/TopBanner';
 import InfoCard from '../InfoCard/InfoCard';
 import Description from '../Description/Description';
+import Footer from '../Footer/Footer';
 
 const LeagueDetail = () => {
 
   const {leagueId} = useParams();
 
   const [league, setLeague] = useState({});
-  const { strTwitter, strFacebook, strYoutube } = league;
-    
-  
-  
-  // Social Media link conditions and generate the link
-  const twitterCondition = !strTwitter ? "www.twitter.com" : strTwitter;
-  const twitter = `https://${twitterCondition}`;
-  const facebookCondition = !strFacebook ? "www.facebook.com" : strFacebook;
-  const facebook = `https://${facebookCondition}`;
-  const youtubeCondition = !strYoutube ? "www.youtube.com" : strYoutube;
-  const youtube = `https://${youtubeCondition}`;
 
   useEffect(() => {
     const singleLinkURL = `https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${leagueId}`;
@@ -47,25 +35,7 @@ const LeagueDetail = () => {
       </div>
 
       {/* Footer Social Icons */}
-      <footer className="text-center">
-        <div className="footer-icon">
-          <a href={twitter} target="_blank" rel="noreferrer">
-            <span className="icon twitter">
-              <FontAwesomeIcon icon={faTwitter} />
-            </span>
-          </a>
-          <a href={facebook} target="_blank" rel="noreferrer">
-            <span className="icon facebook">
-              <FontAwesomeIcon icon={faFacebookF} />
-            </span>
-          </a>
-          <a href={youtube} target="_blank" rel="noreferrer">
-            <span className="icon youtube">
-              <FontAwesomeIcon icon={faYoutube} />
-            </span>
-          </a>
-        </div>
-      </footer>
+      <Footer league={league}></Footer>
     </>
   );
 };
